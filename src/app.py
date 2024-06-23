9d34ee1b4e8fcad2c19fa8c544c4793c005a56ed
NelsonAVN <119307206+NelsonAVN@users.noreply.github.com>
2024-06-23 23:31:11 +0000

    my first commit

--- src/app.py
+++ src/app.py
@@ -26,17 +26,59 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def get_members():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
     response_body = {
-        "hello": "world",
         "family": members
     }
-
-
     return jsonify(response_body), 200
+
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+
+    # this is how you can use the Family datastructure by calling its methods
+    member = jackson_family.get_member(member_id)
+    
+    if not member:
+        return jsonify({"Error" : "the member doesn't exist"}), 404 
+    return jsonify(member), 200
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    if not request.is_json:
+        return jsonify({"error": "Request must be JSON"}), 400
+
+    request_data = request.get_json()
+    
+    if 'first_name' not in request_data or 'age' not in request_data or 'lucky_numbers' not in request_data:
+        return jsonify({"error": "Missing data"}), 400
+
+    try:
+        new_member = {
+            "first_name": request_data['first_name'],
+            "age": request_data['age'],
+            "lucky_numbers": request_data['lucky_numbers']
+        }
+    except KeyError as e:
+        return jsonify({"error": f"Missing {e.args[0]}"}), 400
+
+    jackson_family.add_member(new_member)
+    return jsonify(new_member), 200
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+        member = jackson_family.get_member(member_id)
+        if not member: 
+            return jsonify({"error": "Member not found"}), 404
+        jackson_family.delete_member(member_id)
+        return jsonify({"message": "Member deleted successfully"}), 200
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"}), 404
+    
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
